f39dd4f535d8780287dfcbfd8a50b59179afaf22
TeuerStein <xenkai123456789@gmail.com>
2022-12-22 23:42:01 +0100

    Initial commit

---
+++ GlobalIntranet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GlobalIntranet
+//
+//  Created by Teuer Stein on 22/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GlobalIntranet/GlobalIntranetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalIntranetApp.swift
+//  GlobalIntranet
+//
+//  Created by Teuer Stein on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GlobalIntranetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
