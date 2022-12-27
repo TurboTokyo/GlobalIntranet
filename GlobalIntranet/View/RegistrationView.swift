0c4c6c25dfb647d56ebb9c07280ca04fe242c999
TeuerStein <xenkai123456789@gmail.com>
2022-12-27 17:10:47 +0100

    The previous commit wasn't about a fully completed UI from the Registration screen, now it is

--- GlobalIntranet/View/RegistrationView.swift
+++ GlobalIntranet/View/RegistrationView.swift
@@ -6,61 +6,48 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct RegistrationView: View {
+    // User details
     @State var username: String = ""
     @State var emailID: String = ""
     @State var password: String = ""
+    @State var userBio: String = ""
+    @State var userBioLink: String = ""
+    @State var userProfilePicData: Data?
+    
+    // View properties
+    @Environment(\.dismiss) var dismiss
+    @State var showImagePicker: Bool = false
+    @State var photoItem: PhotosPickerItem?
     
     var body: some View {
         VStack(spacing: 10){
-            Text("Lets Sign you in")
+            Text("Lets Register\nAccount")
                 .font(.largeTitle.bold())
                 .hAlign(.leading)
             
-            Text("Welcome Back, \nYou have been missed")
+            Text("What's up, User ?")
                 .font(.title3)
                 .hAlign(.leading)
             
-            VStack(spacing: 12){
-                TextField("Username", text: $username)
-                    .textContentType(.emailAddress)
-                    .border(1, .gray.opacity(0.5))
-                    .padding(.top, 25)
-                
-                TextField("Email", text: $emailID)
-                    .textContentType(.emailAddress)
-                    .border(1, .gray.opacity(0.5))
-                
-                SecureField("Password", text: $password)
-                    .textContentType(.emailAddress)
-                    .border(1, .gray.opacity(0.5))
-                
-                Button("Reset password?", action: {})
-                    .font(.callout)
-                    .fontWeight(.medium) // That wasn't an issue feature works from iOS 16.0, so I had to update Xcode
-                    .tint(.black)
-                    .hAlign(.trailing)
-                
-                Button {
-                    
-                } label: {
-                    Text("Sign up")
-                        .foregroundColor(.white)
-                        .hAlign(.center)
-                        .fillView(.black)
+            // For smaller size optimization
+            ViewThatFits {
+                ScrollView(.vertical, showsIndicators: false) {
+                    HelperView()
                 }
-                .padding(.top, 10)
+                
+                HelperView()
             }
             
-            // Registration Button
-            
+            // Login Button
             HStack {
                 Text("Already have an account ?")
                     .foregroundColor(.gray)
                 
                 Button("Login Now") {
-                    
+                    dismiss()
                 }
                 .fontWeight(.bold)
                 .foregroundColor(.black)
@@ -70,5 +57,84 @@ struct RegistrationView: View {
         }
         .vAlign(.top)
         .padding(15)
+        .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
+        .onChange(of: photoItem) { newValue in
+            // Extracting UIImage from PhotoItem
+            if let newValue {
+                Task {
+                    do {
+                        guard let imageData = try await newValue.loadTransferable(type: Data.self) else { return }
+                        
+                        // UI ust be updated on main thread
+                        await MainActor.run(body: {
+                            userProfilePicData = imageData
+                        })
+                    } catch {
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func HelperView() -> some View {
+        VStack(spacing: 12){
+            ZStack {
+                if let userProfilePicData, let image = UIImage(data: userProfilePicData) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } else {
+                    Image("NullProfile")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                }
+            }
+            .frame(width: 85, height: 85)
+            .clipShape(Circle())
+            .contentShape(Circle())
+            .onTapGesture {
+                showImagePicker.toggle()
+            }
+            .padding(.top, 25)
+            
+            TextField("Username", text: $username)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            TextField("Email", text: $emailID)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            SecureField("Password", text: $password)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            TextField("About You", text: $userBio, axis: .vertical)
+                .frame(minHeight: 100, alignment: .top)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            TextField("Bio Link (Optional)", text: $userBioLink)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            Button("Reset password?", action: {})
+                .font(.callout)
+                .fontWeight(.medium)
+                .tint(.black)
+                .hAlign(.trailing)
+            
+            Button {
+                
+            } label: {
+                Text("Sign up")
+                    .foregroundColor(.white)
+                    .hAlign(.center)
+                    .fillView(.black)
+            }
+            .padding(.top, 10)
+        }
     }
 }
